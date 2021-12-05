aebde6b9332db5a03a001e28964b7b210077c973
Valentin Pimenov <valker.rus@gmail.com>
2021-12-05 22:28:47 +0300

    update build.cmd to use lua-script

--- build.cmd
+++ build.cmd
@@ -1,4 +1,5 @@
-npm run build
-aws s3 sync ./build/ s3://foresight.56-studio.com --delete
-aws s3api put-object --bucket foresight.56-studio.com --key bundle.74590.esm.js --content-type text/javascript --body build/bundle.74590.esm.js
-aws s3api put-object --bucket foresight.56-studio.com --key bundle.bc616.js     --content-type text/javascript --body build/bundle.bc616.js
+lua ga-build.lua
+rem npm run build
+rem aws s3 sync ./build/ s3://foresight.56-studio.com --delete
+rem aws s3api put-object --bucket foresight.56-studio.com --key bundle.74590.esm.js --content-type text/javascript --body build/bundle.74590.esm.js
+rem aws s3api put-object --bucket foresight.56-studio.com --key bundle.bc616.js     --content-type text/javascript --body build/bundle.bc616.js
