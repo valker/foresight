8bf3a6be2e8ed92a79cc97d52fa249e3d7058191
Valentin Pimenov <valker.rus@gmail.com>
2023-03-07 01:08:34 +0300

    update build scripts (code styles)

--- ga-build.lua
+++ ga-build.lua
@@ -1,22 +1,26 @@
--- print(arg[1])
--- local bucket = arg[1] == "ga" and "ga.56-studio.com" or "foresight.56-studio.com"
 
 local lfs = require("lfs")
-
 local bucket = "foresight" -- in yandex cloud
-print(bucket)
 
 -- собираем проект
 os.execute("npm run build")
 
 -- загружаем все файлы кроме js
-os.execute("aws s3 sync ./build/ s3://" .. bucket .. " --delete --exclude *.js --endpoint-url=https://storage.yandexcloud.net")
+os.execute("aws s3 sync ./build/ s3://" .. bucket
+		.. " --delete"
+		.. " --exclude *.js"
+		.. " --endpoint-url=https://storage.yandexcloud.net")
 
 -- загружаем файлы js
-for fl in lfs.dir( "build" ) do
-	if string.match(fl, ".+\.js$") then
-		print(fl)
-		local command_line = "aws s3api put-object --bucket " .. bucket .. " --key " .. fl .. " --content-type text/javascript --endpoint-url=https://storage.yandexcloud.net --body build/" .. fl
+for filename in lfs.dir( "build" ) do
+	if string.match(filename, ".+\.js$") then
+		print(filename)
+		local command_line = "aws s3api put-object"
+				.. " --bucket "     .. bucket
+				.. " --key "        .. filename
+				.. " --content-type text/javascript"
+				.. " --endpoint-url=https://storage.yandexcloud.net"
+				.. " --body build/" .. filename
 		os.execute( command_line )
 	end
 end
